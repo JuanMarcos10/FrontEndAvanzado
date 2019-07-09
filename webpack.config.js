448b917f452a11bb28a4f2154ccb69ea68f3dc06
Juan Marcos <juanmarcosmarinperez@gmail.com>
2019-07-09 20:06:23 +0200

    Estilos scss y Dev Server incluidos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'source-map',
     mode: 'development',
     entry: {
         app: path.join(__dirname, 'src', 'main.js'),
@@ -13,13 +15,39 @@ module.exports = {
     module:{
         rules:[{
             test:/\.scss$/,
-            use:['style-loader', 'css-loader', 'sass-loader']
-        }]
+            use:[
+                'style-loader',
+                {
+                    loader:'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+            ],
+        },
+    ],
     },
     plugins: [
         new htmlPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
-
+    devServer: {
+        open: true,
+        overlay: true,
+        port: 3000,
+        hot: true,
+        contentBase: [
+            path.join(__dirname, 'src'),
+            path.join(__dirname, 'src', 'templates'),
+        ],
+        watchContentBase: true,
+    },
 }; 
